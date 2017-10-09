69b61766c23dcf2f0191cfaac220d9eadb9d00b1
Luciano Lopes <lucianolpsf@gmail.com>
2017-10-08 21:58:58 -0300

    Adição de telas.

--- App.js
+++ App.js
@@ -1,21 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Expo from 'expo';
+import AppSetup from './src/setup';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Olá! Hackathon</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default AppSetup;
